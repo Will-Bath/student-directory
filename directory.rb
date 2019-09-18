1b592765b407ca4cf1f4225f3037a532b93e6c24
Will <william.bath@btconnect.com>
2019-09-18 15:59:40 +0100

    Added user inputs of student names, added comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+=begin
 students = [
   {name: "Dr. Hannibal Lecter", cohort: :november},
   {name: "Darth Vader", cohort: :november},
@@ -12,14 +13,29 @@ students = [
   {name: "Norman Bates", cohort: :november}
 ]
 
+=end
+
+def input_students # defining function for user input of student names
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = [] # Creating empty array called students for inputted student names to go in
+  name = gets.chomp # Naming user inputted student names
+  while !name.empty? do # Execute repeatedly as long as name variable is not empty; ! means negation; if empty, move on
+    students << {name: name, cohort: :november} # << is shovel operator which puts things in the array students with hash characteristics
+    puts "Now we have #{students.count} students." # embedding current student count after each name input
+    name = gets.chomp
+  end
+  students # When name is empty, output the array 'students'
+end
+
 def print_header
   puts "The Students of Villains Academy"
   puts "--------------"
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each do |student| # For each of the students, using the argument student (in red)
+    puts "#{student[:name]} (#{student[:cohort]} cohort)" # Will Bath (november cohort)
   end
 end
 
@@ -27,6 +43,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
